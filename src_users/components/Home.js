eb6e6324267be7979d88f35ca732c4fb1ae132f2
Mark Bame <markbame.martires@gmail.com>
2017-06-15 17:49:30 +0800

    implemented progressive web app in the boilerplate

---
+++ src_users/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+// Home page component
+export default class Home extends React.Component {
+  // render
+  render() {
+    return (
+      <div className="page-home">
+       Home
+      </div>
+    );
+  }
+}
---
+++ src_users/components/UserEdit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+// Home page component
+export default class About extends React.Component {
+  // render
+  render() {
+    return (
+      <div className="page-home">
+       About
+      </div>
+    );
+  }
+}
